2d2f1812807ca89266641d5534ef9f7c3d1db155
Hriday Halder <ridoyhalder.bd.200@gmail.com>
2025-07-30 13:43:44 +0600

    User Fronted Complete

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Hero from '../components/Hero'
 import FeatureDestination from '../components/FeatureDestination'
 import ExclusiveOffer from '../components/ExclusiveOffer'
